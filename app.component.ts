e9579b0db0cc2e69202ccd6db4368f38e397ff8d
AtifRiaz-uet <109757936+AtifRiaz-uet@users.noreply.github.com>
2022-08-12 00:20:58 +0500

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  _title:string='';
+  _description:string='';
+
+  // title!:string;
+  // description!:string;
+  info_dict!:{};
+  info_list:any[]=[];
+
+  submit()
+  {
+    if (this._title !== '' && this._description !== '')
+    {
+      console.log("clicked");
+    this.info_dict={'title':this._title,'desp':this._description};
+    this.info_list.push(this.info_dict);
+    this._title='';
+    this._description='';
+    }
+    else{
+      alert("Something is Missing!")
+    }
+    
+  }
+}
